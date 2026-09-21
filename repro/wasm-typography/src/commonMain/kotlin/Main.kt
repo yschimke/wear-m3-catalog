b14bc876911609ccc40a38894ee9911030de2f34
Yuri Schimke <yuri@schimke.ee>
2026-09-21 22:00:47 +0100

    fix: await Skiko before Wasm document capture (#566)

--- repro/wasm-typography/src/commonMain/kotlin/Main.kt
+++ repro/wasm-typography/src/commonMain/kotlin/Main.kt
@@ -1,6 +1,22 @@
 import androidx.wear.compose.material3.Typography
+import kotlinx.browser.window
+import org.jetbrains.skiko.InternalSkikoApi
+import org.jetbrains.skiko.wasm.awaitSkiko
 
+@OptIn(InternalSkikoApi::class)
 fun main() {
+  if (window.location.search == "?awaitSkiko") {
+    println("awaiting Skiko")
+    awaitSkiko.then {
+      constructTypography()
+      null
+    }
+  } else {
+    constructTypography()
+  }
+}
+
+private fun constructTypography() {
   println("before Typography()")
   val typography = Typography()
   println("after Typography(): ${typography.bodyLarge.fontSize}")
