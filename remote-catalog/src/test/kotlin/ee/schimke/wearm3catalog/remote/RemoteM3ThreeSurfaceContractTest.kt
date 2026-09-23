47469edc8571f923209d780fca3420566e98b481
Yuri Schimke <yuri@schimke.ee>
2026-09-23 20:30:36 +0100

    test(remote-m3): update embedded player pin guard

--- remote-catalog/src/test/kotlin/ee/schimke/wearm3catalog/remote/RemoteM3ThreeSurfaceContractTest.kt
+++ remote-catalog/src/test/kotlin/ee/schimke/wearm3catalog/remote/RemoteM3ThreeSurfaceContractTest.kt
@@ -143,7 +143,7 @@ class RemoteM3ThreeSurfaceContractTest {
         "cmp${port.getValue("portRevision").jsonPrimitive.content.padStart(2, '0')}"
 
     assertThat(published).isEqualTo(expected)
-    assertThat(versions).contains("rcEmbeddedPlayer = \"1.69.0\"")
+    assertThat(versions).contains("rcEmbeddedPlayer = \"1.70.0\"")
   }
 
   private fun repositoryRoot(): File {
